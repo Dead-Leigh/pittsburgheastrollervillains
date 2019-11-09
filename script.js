3a5a61ca951a217e417465cfae4a1ee4ca77dc61
Leigh Carr <leighcarr@Leighs-MBP.hsd1.pa.comcast.net>
2019-11-09 13:05:49 -0500

    slideshow JS set up

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var i = 0;
+var images = [];
+var time = 3000;
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+//image list
+images[0] = "img/bluestreaklogo.jpg";
+images[1] = "img/galslogo.jpg";
+images[2] = "img/bluestreaklogo.jpg";
 
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("slideshow");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+
+// fun to change the image
+function changeImg(){
+  document.slide.src = images[i];
+  if(i < images.length - 1){
+    i++;
+  } else {
+    i = 0;
   }
-  x[slideIndex-1].style.display = "block";
+  setTimeout("changeImg()", time);
 }
+
+window.onload = changeImg;
+
+// showDivs(slideIndex);
+//
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+//
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("slideshow");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length} ;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   x[slideIndex-1].style.display = "block";
+// }
